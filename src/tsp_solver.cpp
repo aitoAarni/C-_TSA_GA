806a92605bb7d7a7e3b9ba1e8d395930ba47e3f7
aitoAarni <aarni.rechardt@gmail.com>
2026-05-16 19:07:36 +0300

    population creation works

--- src/tsp_solver.cpp
+++ src/tsp_solver.cpp
@@ -3,13 +3,14 @@
 #include <fstream>
 #include <string>
 #include <string_view>
+#include <numeric>
+#include <algorithm>
+#include <random>
 #include "utils.hpp"
 
 constexpr int MUTATION_RATE {5}; // in percentages
 const std::string input_file {"../run/input.dat"};
 
-// TODO: City data generator
-// TODO: read data to vector, to get cities and their locations
 // TODO: generate initial mutations (and distributed them to threads) 
 
 // in threads
@@ -34,8 +35,28 @@ std::vector<City> read_input(const std::string& filename) {
     return cities;
 }
 
+std::vector<std::vector<int>> generate_population(const int city_count, const int path_count) {
+    std::vector<int> base_route(city_count);
+    std::iota(base_route.begin(), base_route.end(), 0);
+    std::random_device rd;
+    std::mt19937 gen(rd());
+
+    std::vector<std::vector<int>> population(path_count);
+    for (int i {0}; i < path_count; i++) {
+        auto new_route = base_route;
+        std::shuffle(new_route.begin(), new_route.end(), gen);
+        population[i] = new_route;
+    }
+    return population;
+}
+
 int main() {
-    auto cities {read_input(input_file)};
-    print_vector(cities);
+    auto city_locations {read_input(input_file)};
+    std::size_t n {city_locations.size()};
+
+    auto population {generate_population(n, 5)};
+    std::cout << " " << population;
+
+
     return 0;
 }
--- src/utils.hpp
+++ src/utils.hpp
@@ -3,9 +3,18 @@
 #include <vector>
 
 template <typename T>
+std::ostream& operator<<(std::ostream& os, const std::vector<T>& vec) {
+    for (const T& value : vec) {
+        os << value << " ";
+    }
+    os << "\n";
+    return os;
+}
+
+template <typename T>
 void print_vector(std::vector<T>& vec) {
     for (const T& value : vec) {
-        std::cout << value;
+        std::cout << value << " ";
     }
     std::cout << "\n";
 }
@@ -17,5 +26,5 @@ struct City {
 };
 
 std::ostream& operator<<(std::ostream& os, const City& city) {
-    return os << city.x << " " << city.y << "\n";
+    return os << city.x << " " << city.y;
 }
