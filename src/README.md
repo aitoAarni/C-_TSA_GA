e23fd41ff899adef6078312f38829d378edbbd1d
aitoAarni <aarni.rechardt@gmail.com>
2026-05-16 15:31:43 +0300

    add readme and todo

---
+++ src/README.md
@@ -0,0 +1,10 @@
+# Instructions for Running the Code
+
+These instructions are for Linux environments. The commands work if you are executing them from the root directory of the project.
+
+### Generetaing input data
+To generate input data, run the following command in the terminal:
+
+```bash
+python3 src/generate_input.py
+```
---
+++ src/tsp_solver.cpp
@@ -0,0 +1,15 @@
+#include <vector>
+#include <iostream>
+
+constexpr int MUTATION_RATE {5}; // in percentages
+
+// TODO: City data generator
+// TODO: read data to vector, to get cities and their locations
+// TODO: generate initial mutations (and distributed them to threads) 
+
+// in threads
+// TODO: generate next generation, choose from current population a subset and find 2 best distances,
+// and create a child
+
+// DO this 
+
